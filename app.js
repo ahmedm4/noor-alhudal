9fb70e8de8a8e55087c7f2fceb78383d3c7acfad
ahmedm4 <newtech.ahm@gmail.com>
2025-12-05 21:40:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
